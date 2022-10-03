748809b0c786072af1eeb61cfa8e2d394bbf82a9
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-10-04 01:17:33 +0530

    initial commit

---
+++ MERN/MERN_CRUD/client/src/components/CreatePost.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class CreatePost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topic: "",
+      description: "",
+      postCategory: "",
+    };
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      ...this.state,
+      [name]: value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const { topic, description, postCategory } = this.state;
+    const data = {
+      topic: topic,
+      description: description,
+      postCategory: postCategory,
+    };
+    console.log(data);
+    axios.post("/post/save", data).then((res) => {
+      if (res.data.success) {
+        this.setState({
+          topic: "",
+          description: "",
+          postCategory: "",
+        });
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="col-md-8 mt-4 mx-auto">
+        <h1 className="h3 mb-3 font-weight-normal">Create New Post</h1>
+        <form className="need-validation" noValidate>
+          <div className="form-group" style={{ marginBottom: "15px" }}>
+            <label style={{ marginBottom: "5px" }}>Topic</label>
+            <input
+              type="text"
+              className="form-control"
+              name="topic"
+              placeholder="Enter Topic"
+              value={this.state.topic}
+              onChange={this.handleInputChange}
+            />
+          </div>
+
+          <div className="form-group" style={{ marginBottom: "15px" }}>
+            <label style={{ marginBottom: "5px" }}>Description</label>
+            <input
+              type="text"
+              className="form-control"
+              name="description"
+              placeholder="Enter Description"
+              value={this.state.description}
+              onChange={this.handleInputChange}
+            />
+          </div>
+
+          <div className="form-group" style={{ marginBottom: "15px" }}>
+            <label style={{ marginBottom: "5px" }}>Post Category</label>
+            <input
+              type="text"
+              className="form-control"
+              name="postCategory"
+              placeholder="Enter Post Category"
+              value={this.state.postCategory}
+              onChange={this.handleInputChange}
+            />
+          </div>
+
+          <button
+            className="btn btn-success"
+            type="submit"
+            style={{ marginTop: "15px" }}
+            onClick={this.onSubmit}
+          >
+            <i className="fa fa-check-square"></i>&nbsp;Save
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
---
+++ MERN/MERN_CRUD/client/src/components/EditPost.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import axios from "axios";
+export default class EditPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topic: "",
+      description: "",
+      postCategory: "",
+    };
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      ...this.state,
+      [name]: value,
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const id = this.props.match.params.id;
+    const { topic, description, postCategory } = this.state;
+    const data = {
+      topic: topic,
+      description: description,
+      postCategory: postCategory,
+    };
+    console.log(data);
+    axios.put(`/post/update/${id}`, data).then((res) => {
+      if (res.data.success) {
+        alert("Post Updated Successfully");
+        this.setState({
+          topic: "",
+          description: "",
+          postCategory: "",
+        });
+      }
+    });
+  };
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    axios.get(`/post/${id}`).then((res) => {
+      if (res.data.success) {
+        this.setState({
+          topic: res.data.post.topic,
+          description: res.data.post.description,
+          postCategory: res.data.post.postCategory,
+        });
+
+        console.log(this.state.post);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="col-md-8 mt-4 mx-auto">
+        <h1 className="h3 mb-3 font-weight-normal">Create New Post</h1>
+        <form className="need-validation" noValidate>
+          <div className="form-group" style={{ marginBottom: "15px" }}>
+            <label style={{ marginBottom: "5px" }}>Edit Topic</label>
+            <input
+              type="text"
+              className="form-control"
+              name="topic"
+              placeholder="Enter Topic"
+              value={this.state.topic}
+              onChange={this.handleInputChange}
+            />
+          </div>
+
+          <div className="form-group" style={{ marginBottom: "15px" }}>
+            <label style={{ marginBottom: "5px" }}>Edit Description</label>
+            <input
+              type="text"
+              className="form-control"
+              name="description"
+              placeholder="Enter Description"
+              value={this.state.description}
+              onChange={this.handleInputChange}
+            />
+          </div>
+
+          <div className="form-group" style={{ marginBottom: "15px" }}>
+            <label style={{ marginBottom: "5px" }}>Edit Post Category</label>
+            <input
+              type="text"
+              className="form-control"
+              name="postCategory"
+              placeholder="Enter Post Category"
+              value={this.state.postCategory}
+              onChange={this.handleInputChange}
+            />
+          </div>
+
+          <button
+            className="btn btn-success"
+            type="submit"
+            style={{ marginTop: "15px" }}
+            onClick={this.onSubmit}
+          >
+            <i className="fa fa-check-square"></i>&nbsp;Update
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
